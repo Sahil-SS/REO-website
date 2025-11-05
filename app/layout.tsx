d4e54bfb761f1e00bd2b63e72170fd7f20bb02ca
Sahil Sinha <sinhasahil272004@gmail.com>
2025-11-05 20:53:19 +0530

    added videos

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `} 
-      >
-        {children}<SpeedInsights />
+      ><Analytics />
+      <SpeedInsights />
+        {children}
       </body>
     </html>
   );
